df43b97c762331ce3d81dc94848398b7e31766e3
Riddhiman Chowdhury <Rudrasir123@outlook.com>
2023-04-27 21:23:10 +0530

    construction of side and navbar almost complete. Backend of buttons are not set (some are done).

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,6 @@ import {
   Avatar,
   Box,
   Button,
-  ButtonGroup,
   Container,
   CssBaseline,
   Divider,
@@ -21,8 +20,12 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Menu,
+  MenuItem,
+  MenuList,
   SwipeableDrawer,
   Toolbar,
+  Tooltip,
   Typography,
 } from "@mui/material";
 import { MdMenu, MdSearch, MdShieldMoon, MdSunny } from "react-icons/md";
@@ -39,69 +42,110 @@ import { HiUserAdd, HiUserCircle, HiMoon } from "react-icons/hi";
 
 export default function Navbar() {
   const { data: session } = useSession();
+
+  // ui states
   const [isDark, setIsDark] = useState(false);
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [isLandscape, setIsLandscape] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const profileMenuOpen = Boolean(anchorEl);
+  useEffect(() => {
+    const query = window.matchMedia("(min-width:992px)");
+    window.addEventListener("load", () => setIsLandscape(query.matches));
+    window.addEventListener("resize", (ev) => setIsLandscape(query.matches)); // whether the size of the window matches the required media
+  }, []);
+
   // React.useEffect(() => NavbarConfig(document.getElementById("mainNav")));
   return (
-    <AppBar
-      variant="elevation"
-      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      className="bg-blue-50"
-      component="nav"
-    >
-      <Container className="my-2 flex flex-row justify-between">
-        <CssBaseline />
-        {/* sidebar button */}
-        <IconButton
-          onClick={() => setDrawerOpen(!drawerOpen)}
-          className="text-zinc-900"
-          aria-label="open drawer"
-          edge="start"
-        >
-          <MdMenu className="h-6 w-6" />
-        </IconButton>
-        <div className="flex flex-row justify-evenly gap-3">
-          <div>
-            <IconButton>
-              <MdSearch />
-            </IconButton>
+    <>
+      <AppBar
+        variant="elevation"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        className="bg-blue-50"
+        component="nav"
+      >
+        <Container className="my-2 flex flex-row justify-between">
+          <CssBaseline />
+          {/* sidebar button */}
+          <IconButton
+            onClick={() => setDrawerOpen(!drawerOpen)}
+            className="text-zinc-900"
+            aria-label="click here to open navbar"
+            edge="start"
+          >
+            <MdMenu className="h-6 w-6" />
+          </IconButton>
+          <div className="flex flex-row justify-evenly gap-3">
+            <div>
+              <IconButton>
+                <MdSearch />
+              </IconButton>
+            </div>
+            {/* dark mode */}
+            <div>
+              <IconButton onClick={() => setIsDark(!isDark)}>
+                {isDark ? <MdSunny /> : <HiMoon />}
+              </IconButton>
+            </div>
+            {session ? (
+              <>
+                {/* avatar */}
+                <Tooltip title="Profile">
+                  <IconButton onClick={(ev) => setAnchorEl(ev.currentTarget)}>
+                    <Avatar sx={{ width: "3rem", height: "3rem" }} variant="circular">
+                      <Image
+                        alt=""
+                        fill
+                        // className="m-auto inline-block h-[5rem] w-[5rem] rounded-full ring-2 ring-white"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      />
+                    </Avatar>
+                  </IconButton>
+                </Tooltip>
+
+                {/* profile menu */}
+                <Menu
+                  anchorEl={anchorEl}
+                  MenuListProps={{ variant: "menu" }}
+                  variant="menu"
+                  open={profileMenuOpen}
+                  onClick={() => {
+                    setAnchorEl(null);
+                  }}
+                  onClose={() => setAnchorEl(null)}
+                >
+                  <MenuList>
+                    <Divider />
+                    <MenuItem LinkComponent={Link} href="/api/auth/logout">
+                      <ListItemIcon>
+                        <MdLogin className="h-5 w-5 text-zinc-800 group-hover:text-zinc-50" />
+                      </ListItemIcon>
+                      <ListItemText>Logout</ListItemText>
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </>
+            ) : (
+              <>
+                <Button
+                  LinkComponent={Link}
+                  href="/auth/login"
+                  className="bg-blue-700 text-blue-50"
+                >
+                  Login
+                </Button>
+                <Button
+                  LinkComponent={Link}
+                  href="/auth/register"
+                  className="bg-stone-800 text-stone-50"
+                >
+                  Sign Up
+                </Button>
+              </>
+            )}
           </div>
-          {/* dark mode */}
-          <div>
-            <IconButton onClick={() => setIsDark(!isDark)}>
-              {isDark ? <MdSunny /> : <HiMoon />}
-            </IconButton>
-          </div>
-          {session ? (
-            /* avatar */
-            <Avatar sx={{ width: "3rem", height: "3rem" }} variant="circular">
-              <Image
-                alt=""
-                fill
-                // className="m-auto inline-block h-[5rem] w-[5rem] rounded-full ring-2 ring-white"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              />
-            </Avatar>
-          ) : (
-            <>
-              <Button
-                LinkComponent={Link}
-                href="/auth/login"
-                className="bg-blue-700 text-blue-50"
-              >
-                Login
-              </Button>
-              <Button
-                LinkComponent={Link}
-                href="/auth/register"
-                className="bg-stone-800 text-stone-50"
-              >
-                Sign Up
-              </Button>
-            </>
-          )}
-        </div>
-      </Container>
+        </Container>
+      </AppBar>
 
       {/* sidebar */}
       <SwipeableDrawer
@@ -109,8 +153,7 @@ export default function Navbar() {
         onOpen={() => setDrawerOpen(true)}
         onClose={() => setDrawerOpen(false)}
         draggable="true"
-        variant="temporary"
-        slotProps={{ backdrop: "w-72 bg-pink-900" }}
+        variant={isLandscape ? "permanent" : "temporary"}
         className="w-72"
         sx={{ width: "18rem", display: "block", marginTop: "64px" }}
       >
@@ -118,53 +161,167 @@ export default function Navbar() {
           sx={{ width: "18rem", height: "100%", marginTop: "64px" }}
           className="flex h-full flex-col bg-blue-50"
         >
-          {/* profile area */}
-          <div className="my-8 flex flex-col justify-center">
-            <div className="flex flex-row items-center justify-center">
-              <Avatar
-                sx={{ width: "5rem", height: "5rem" }}
-                variant="circular"
-                className="text-center"
-              >
-                <Image
-                  alt=""
-                  fill
-                  // className="m-auto inline-block h-[5rem] w-[5rem] rounded-full ring-2 ring-white"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                />
-              </Avatar>
-            </div>
-          </div>
-          <Divider />
-          <List className="px-4">
+          {session && (
+            <>
+              {/* profile area */}
+              <Box className="my-8 flex flex-col justify-center gap-4">
+                {/* avatar */}
+                <Box className="flex flex-row items-center justify-center">
+                  <Avatar
+                    sx={{ width: "5rem", height: "5rem" }}
+                    variant="circular"
+                    className="text-center"
+                  >
+                    <Image
+                      alt=""
+                      fill
+                      // className="m-auto inline-block h-[5rem] w-[5rem] rounded-full ring-2 ring-white"
+                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    />
+                  </Avatar>
+                </Box>
+                {/* user details */}
+                <Box className="mx-4 flex flex-col gap-2">
+                  {/* Full name */}
+                  <Typography
+                    textAlign="center"
+                    component="h6"
+                    className="text-xl font-semibold"
+                    variant="h6"
+                  >
+                    {session.user.FullName}
+                  </Typography>
+                  {/* nickname */}
+                  <Typography
+                    textAlign="center"
+                    component="p"
+                    className="font-serif text-lg"
+                  >
+                    {session.user.username}
+                  </Typography>
+                  {/* email */}
+                  <Typography textAlign="center" component="p" className="italic ">
+                    {session.user.email}
+                  </Typography>
+                </Box>
+              </Box>
+              <Divider />
+            </>
+          )}
+          <List className="flex flex-col gap-1 px-4">
             {/* home */}
-            <ListItemButton className="rounded-md shadow-zinc-500 hover:shadow-md">
+            <ListItemButton
+              LinkComponent={Link}
+              href="/"
+              TouchRippleProps={{ className: "text-zinc-300" }}
+              className="group rounded-md shadow-zinc-700 outline-transparent hover:bg-zinc-900 hover:shadow-xl"
+            >
               <ListItemIcon>
-                <HiHome className="h-5 w-5" />
+                <HiHome className="h-5 w-5 text-zinc-800 group-hover:text-zinc-50" />
               </ListItemIcon>
-              <ListItemText className="text-center">Home</ListItemText>
+              <ListItemText className="text-center text-zinc-900 group-hover:text-zinc-50">
+                Home
+              </ListItemText>
             </ListItemButton>
 
             {/* profile */}
-            <ListItemButton className="rounded-md shadow-zinc-500 hover:shadow-md">
+            <ListItemButton
+              LinkComponent={Link}
+              href="/profile"
+              TouchRippleProps={{ className: "text-zinc-300" }}
+              className="group rounded-md shadow-zinc-700 outline-transparent hover:bg-zinc-900 hover:shadow-xl"
+            >
               <ListItemIcon>
-                <HiUserCircle className="h-5 w-5" />
+                <HiUserCircle className="h-5 w-5 text-zinc-800 group-hover:text-zinc-50" />
               </ListItemIcon>
-              <ListItemText className="text-center">Profile</ListItemText>
+              <ListItemText className="text-center text-zinc-900 group-hover:text-zinc-50">
+                Profile
+              </ListItemText>
             </ListItemButton>
 
-            {/* home */}
-            <ListItemButton className="rounded-md shadow-zinc-500 hover:shadow-md">
+            {/* dashboard */}
+            <ListItemButton
+              LinkComponent={Link}
+              href="/dashboard"
+              TouchRippleProps={{ className: "text-zinc-300" }}
+              className="group rounded-md shadow-zinc-700 outline-transparent hover:bg-zinc-900 hover:shadow-xl"
+            >
               <ListItemIcon>
-                <MdDashboard className="h-5 w-5" />
+                <MdDashboard className="h-5 w-5 text-zinc-800 group-hover:text-zinc-50" />
               </ListItemIcon>
-              <ListItemText className="text-center">Dashboard</ListItemText>
+              <ListItemText className="text-center text-zinc-900 group-hover:text-zinc-50">
+                Dashboard
+              </ListItemText>
             </ListItemButton>
           </List>
-
           <Divider />
+
+          {/* post, image, videos contents */}
+          <List className="flex flex-col gap-1 px-4">
+            {/* home */}
+            <ListItemButton
+              LinkComponent={Link}
+              href="/"
+              TouchRippleProps={{ className: "text-zinc-300" }}
+              className="group rounded-md shadow-zinc-700 outline-transparent hover:bg-zinc-900 hover:shadow-xl"
+            >
+              <ListItemIcon>
+                <HiHome className="h-5 w-5 text-zinc-800 group-hover:text-zinc-50" />
+              </ListItemIcon>
+              <ListItemText className="text-center text-zinc-900 group-hover:text-zinc-50">
+                Posts
+              </ListItemText>
+            </ListItemButton>
+
+            {/* profile */}
+            <ListItemButton
+              LinkComponent={Link}
+              href="/profile"
+              TouchRippleProps={{ className: "text-zinc-300" }}
+              className="group rounded-md shadow-zinc-700 outline-transparent hover:bg-zinc-900 hover:shadow-xl"
+            >
+              <ListItemIcon>
+                <HiUserCircle className="h-5 w-5 text-zinc-800 group-hover:text-zinc-50" />
+              </ListItemIcon>
+              <ListItemText className="text-center text-zinc-900 group-hover:text-zinc-50">
+                Profile
+              </ListItemText>
+            </ListItemButton>
+
+            {/* dashboard */}
+            <ListItemButton
+              LinkComponent={Link}
+              href="/dashboard"
+              TouchRippleProps={{ className: "text-zinc-300" }}
+              className="group rounded-md shadow-zinc-700 outline-transparent hover:bg-zinc-900 hover:shadow-xl"
+            >
+              <ListItemIcon>
+                <MdDashboard className="h-5 w-5 text-zinc-800 group-hover:text-zinc-50" />
+              </ListItemIcon>
+              <ListItemText className="text-center text-zinc-900 group-hover:text-zinc-50">
+                Dashboard
+              </ListItemText>
+            </ListItemButton>
+          </List>
+          <Divider />
+          <List className="mb-8">
+            {/* logout */}
+            <ListItemButton
+              LinkComponent={Link}
+              href="/api/auth/signout"
+              TouchRippleProps={{ className: "text-zinc-300" }}
+              className="group rounded-md shadow-zinc-700 outline-transparent hover:bg-zinc-900 hover:shadow-xl"
+            >
+              <ListItemIcon>
+                <MdLogout className="h-5 w-5 text-zinc-800 group-hover:text-zinc-50" />
+              </ListItemIcon>
+              <ListItemText className="text-center text-zinc-900 group-hover:text-zinc-50">
+                Logout
+              </ListItemText>
+            </ListItemButton>
+          </List>
         </Box>
       </SwipeableDrawer>
-    </AppBar>
+    </>
   );
 }
--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,42 +1,39 @@
+import {
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Container,
+  CssBaseline,
+  Icon,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { FaBlog } from "react-icons/fa";
 
 export default function dashboard() {
   return (
-    <div className="container mx-auto mt-8 py-10">
-      <div className="flex w-full flex-col flex-wrap justify-evenly gap-4 lg:flex-row">
-        <div className=" flex flex-row flex-wrap gap-6 rounded-md bg-blue-800 py-4 pr-6 shadow-blue-500 drop-shadow-md">
-          <div className=" px-6 py-4">
-            <FaBlog className="text-7xl text-blue-50" />
-          </div>
-          <div className="flex flex-col flex-wrap gap-4 ">
-            <div>
-              <div>
-                <p className="text-lg text-blue-50">Posts:</p>
-              </div>
-              <div className="">
-                <h3 className="text-right text-6xl text-blue-50">1527</h3>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col gap-4 rounded-md bg-zinc-800 px-6 py-4 shadow-blue-500 drop-shadow-md ">
-          <div>
-            <p className="text-blue-50">Posts:</p>
-          </div>
-          <div className="">
-            <h3 className="text-right text-6xl text-blue-50">1527</h3>
-          </div>
-        </div>
-        <div className="flex flex-col gap-4 rounded-md bg-emerald-700 px-6 py-4 shadow-blue-500 drop-shadow-md ">
-          <div>
-            <p className="text-blue-50">Posts:</p>
-          </div>
-          <div className="">
-            <h3 className="text-right text-6xl text-blue-50">1527</h3>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <Container className="py-10">
+        <CssBaseline />
+        <Card
+          aria-label="posts created"
+          className="flex flex-row gap-2 bg-blue-700 px-6 py-4"
+        >
+          <Box className="text-center">
+            <CardMedia>
+              <Icon>
+                <FaBlog className="h-7 w-7 text-blue-50" />
+              </Icon>
+            </CardMedia>
+          </Box>
+          <Box className="w-full">
+            <CardHeader title="Posts:" />
+            <CardContent>1527</CardContent>
+          </Box>
+        </Card>
+      </Container>
+    </>
   );
 }
