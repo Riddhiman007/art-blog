a5ecc06089cd9f760773ac03371f76810772ece5
Riddhiman007 <91105040+Riddhiman007@users.noreply.github.com>
2023-07-05 09:00:50 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-=======
 
->>>>>>> fc2a61e (deploy)
   images: {
     remotePatterns: [ { hostname: "images.unsplash.com", port: "", pathname: "/**" } ]
   },
